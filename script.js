bfa23a96d1e247a8c5e685b1fdf2253da631dd9d
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-01-18 00:34:02 +0530

    added layout

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log(emojiList);
 const emojiContainer = document.getElementById("emoji_container");
 const emoji_list = [];
-for (let i = 0; i < 10; i++)emoji_list.push(emojiList[i]);
+for (let i = 0; i < 60; i++)emoji_list.push(emojiList[i]);
 console.log(emoji_list);
 const createEmojiCard = (emojiObj) => {
   let div = document.createElement("div");
   div.innerHTML = `
             <p class="emoji">${emojiObj.emoji}</p>
-            <p class="alias">${emojiObj.aliases[0]}</p>
+            <p class="name">${emojiObj.aliases[0].replaceAll('_', " ")}</p>
             <p class="description">${emojiObj.description}</p>
         `
   div.classList.add("emoji_card");
