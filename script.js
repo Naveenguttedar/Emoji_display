ffdac963554579eb2adf3034e9b8115353dc00aa
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-01-17 23:15:52 +0530

    added files

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(emojiList);
+const emojiContainer = document.getElementById("emoji_container");
+const emoji_list = [];
+for (let i = 0; i < 10; i++)emoji_list.push(emojiList[i]);
+console.log(emoji_list);
+const createEmojiCard = (emojiObj) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+            <p class="emoji">${emojiObj.emoji}</p>
+            <p class="alias">${emojiObj.aliases[0]}</p>
+            <p class="description">${emojiObj.description}</p>
+        `
+  div.classList.add("emoji_card");
+  return div;
+}
+//rendering emojisss
+emoji_list.map(emojiObj => {
+  console.log(emojiObj);
+  const emoji_card = createEmojiCard(emojiObj);
+  emojiContainer.appendChild(emoji_card);
+})
