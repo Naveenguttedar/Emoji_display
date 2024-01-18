1728a804db7ceacce59245878ee3fb69b7774a19
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-01-19 00:56:16 +0530

    added filter logic

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-console.log(emojiList);
 const emojiContainer = document.getElementById("emoji_container");
+const emoji_search_input = document.getElementById("serach_emoji_input");
+const btn_search = document.getElementById("btn_search");
 const emoji_list = [];
-for (let i = 0; i < 60; i++)emoji_list.push(emojiList[i]);
-console.log(emoji_list);
+for (let i = 0; i < 120; i++)emoji_list.push(emojiList[i]);
 const createEmojiCard = (emojiObj) => {
   let div = document.createElement("div");
   div.innerHTML = `
@@ -15,7 +15,24 @@ const createEmojiCard = (emojiObj) => {
 }
 //rendering emojisss
 emoji_list.map(emojiObj => {
-  console.log(emojiObj);
   const emoji_card = createEmojiCard(emojiObj);
   emojiContainer.appendChild(emoji_card);
 })
+//seraching emoji
+const searchEmoji = (input, emojiList) => {
+  return emojiList.filter((value) => value.aliases[0].includes(input))
+}
+//rerendering filterd emojisss
+btn_search.addEventListener("click", () => {
+  const seacrchValue = emoji_search_input.value;
+  if (!seacrchValue) alert("Seacrch value can not be Empty.");
+  else {
+    emojiContainer.innerHTML = "";
+    const filterdEmojis = searchEmoji(seacrchValue, emoji_list);
+    filterdEmojis.map(emojiObj => {
+      const emoji_card = createEmojiCard(emojiObj);
+      emojiContainer.appendChild(emoji_card);
+    })
+  }
+})
+
